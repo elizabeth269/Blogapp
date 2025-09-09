02bb1893a2c42ffb59f10fb70b2d9c8f6834dcb2
Elizbeth <elizabethadebayo269@gmail.com>
2025-09-09 05:50:21 +0100

    i created my models and i registered them in my admin

--- myblog/blogapp/models.py
+++ myblog/blogapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)  
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    published = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+        
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
